a72fdb3e5a2976ab6e8f1a525276c7aeec2f6261
cyan <cyanyiyi@foxmail.com>
2017-11-29 22:04:46 +0800

    json2doc

---
+++ main.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const I18N_PATH = path.join(__dirname, 'i18n');
+// console.log(fs);
+// console.log(path);
+console.log(__dirname);
+console.log(I18N_PATH);
