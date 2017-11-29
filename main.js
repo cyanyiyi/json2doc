4c1e7be9535272629159ccb16c6625ec4b68fe54
cyan <cyanyiyi@foxmail.com>
2017-11-30 00:07:01 +0800

    main

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ const path = require('path');
 const I18N_PATH = path.join(__dirname, 'i18n');
 // console.log(fs);
 // console.log(path);
-console.log(__dirname);
-console.log(I18N_PATH);
+// console.log(__dirname);
+// console.log(I18N_PATH);
