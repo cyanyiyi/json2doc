2077bd23a628d99a58e5d1238195e6f24be69c72
cyan <cyanyiyi@foxmail.com>
2017-12-04 00:06:22 +0800

    test

--- i18n/test.js
+++ i18n/test.js
@@ -1,17 +1,17 @@
-{page_title: {
-        404: 'Page does not exist',
-        common: 'The backstage management system of Roamingman',
-        components: '我是汉字'
+const lan = {
+    "page_title": {
+        "404": "Page does not exist",
+        "common": "The backstage management system of Roamingman",
+        "components": "我是汉字"
     },
-    component: {
-        tips: {
-            title: 'Tips',
-            ok: 'Confirm',
-            close: '我也是汉字'
+    "component": {
+        "tips": {
+            "title": "Tips",
+            "close": "我也是汉字"
         },
-        v_login: {
-            titleM: 'Management platform',
-            titleA: 'Account login'
+        "v_login": {
+            "titleM": "Management platform",
+            "titleA": "Account login"
         }
     }
 }
