bd98976a8f617fbc9d145d3f2721de2ba8a50ae6
cyan <cyanyiyi@foxmail.com>
2017-12-03 23:48:33 +0800

    test

---
+++ i18n/test.js
@@ -0,0 +1,21 @@
+/**
+ * Created by yankang on 2017/07/06.
+ */
+const lan = {
+    page_title: {
+        404: 'Page does not exist',
+        common: 'The backstage management system of Roamingman',
+        components: '我是汉字'
+    },
+    component: {
+        tips: {
+            title: 'Tips',
+            ok: 'Confirm',
+            close: '我也是汉字'
+        },
+        v_login: {
+            titleM: 'Management platform',
+            titleA: 'Account login'
+        }
+    }
+}
--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 const fs = require('fs');
 const path = require('path');
 const I18N_PATH = path.join(__dirname, 'i18n');
-const I18N_PATH_EN = path.join(I18N_PATH, 'en-US.js')
+const I18N_PATH_EN = path.join(I18N_PATH, 'en-US.js');
+const I18N_PATH_TEST = path.join(I18N_PATH, 'test.js');
 const LAN_CN = 'zh-CN';
 const LAN_TW = 'zh-TW';
 const LAN_EN = 'en-US';
 const REG_EXP_NOTE = new RegExp("(/\\\*([^*]|[\\\r\\\n]|(\\\*+([^*/]|[\\\r\\\n])))*\\\*+/)|(//.*)","g");  
-console.log(I18N_PATH_EN);
-let strFileData = fs.readFileSync(I18N_PATH_EN, 'utf8');
-console.log(strFileData.match(REG_EXP_NOTE));
-let strFileData2 = strFileData.replace(REG_EXP_NOTE, "");  
-// console.log(strFileData2);
-let parseFileData = JSON.parse(strFileData2);
-// console.log(fileData);
+const REG_EXP_CONST = /const[\s]*lan[\s]*=/g;
+const REG_EXP_EXPORTS = /module.exports[\s]*=[\s]*lan;/g;
+
+console.log(I18N_PATH_TEST);
+// let strFileData = fs.readFileSync(I18N_PATH_EN, 'utf8');
+let strFileData = fs.readFileSync(I18N_PATH_TEST, 'utf8');
+console.log(strFileData);
+// console.log(typeof strFileData);
+// console.log(strFileData.match(REG_EXP_CONST));
+// console.log(strFileData.match(REG_EXP_EXPORTS));
+let strFileDataTmp = strFileData.replace(REG_EXP_NOTE, "");  
+strFileDataTmp = strFileDataTmp.replace(REG_EXP_CONST, "");  
+let strFileDataJson = strFileDataTmp.replace(REG_EXP_EXPORTS, "");  
+console.log(strFileDataJson);
+let parseFileData = JSON.parse(strFileDataJson);
 console.log(parseFileData);
 
