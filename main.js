64fe97ecac7043b16a7bf8eda9362e1f437a2337
unknown <kang.yan@ucloudlink.com>
2017-12-04 20:08:16 +0800

    main

--- main.js
+++ main.js
@@ -48,26 +48,3 @@ function fnDeleteEmptyObj(obj) {
 
 let filterJson = fnFilterZhInEn(testJson);
 let pureTestJson = fnDeleteEmptyObj(filterJson);
-
-
-
-
-// const REG_EXP_NOTE = new RegExp("(/\\\*([^*]|[\\\r\\\n]|(\\\*+([^*/]|[\\\r\\\n])))*\\\*+/)|(//.*)","g");  
-// const REG_EXP_CONST = /const[\s]*lan[\s]*=/g;
-// const REG_EXP_EXPORTS = /module.exports[\s]*=[\s]*lan;/g;
-// const REG_EXP_SINGLE_SEMI = /'([A-Za-z0-9_\.\s。]*)'/g; // 匹配单引号内的所有字符包括 . 。 _和空格
-// const REG_EXP_STR = /(\w+)/g;
-// let strFileData = fs.readFileSync(I18N_PATH_EN, 'utf8');
-// let strFileData = fs.readFileSync(I18N_PATH_TEST, 'utf8');
-// console.log(strFileData);
-// console.log(typeof strFileData);
-// console.log(strFileData.match(REG_EXP_CONST));
-// console.log(strFileData.match(REG_EXP_EXPORTS));
-// let strFileDataTmp = strFileData.replace(REG_EXP_NOTE, ""); // 去掉注释
-// strFileDataTmp = strFileDataTmp.replace(REG_EXP_CONST, ""); // 去掉声明
-// strFileDataTmp = strFileDataTmp.replace(REG_EXP_EXPORTS, ""); // 去掉exports  
-// strFileDataTmp = strFileDataTmp.replace(REG_EXP_SINGLE_SEMI, '"$1"');  // 单引号转双引号
-// let strFileDataJson = strFileDataTmp.replace(REG_EXP_STR, "");  // 给不带引号的key加上双引号
-// console.log(strFileDataJson);
-// let parseFileData = JSON.parse(strFileDataJson);
-// console.log(parseFileData);
