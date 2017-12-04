eaec740ba983cfa6c03b1e65230eee3fd0d031bd
unknown <kang.yan@ucloudlink.com>
2017-12-04 20:29:27 +0800

    main

---
+++ i18n/test-zh.js
@@ -0,0 +1,43 @@
+const lan = {
+    page_title: {
+        404: '404',
+        common: "供应",
+        components: "我是汉字",
+        components1: "我是汉字1",
+        components2: "我是汉字2",
+        components3: "我是汉字3",
+        components4: "我是汉字4",
+    },
+    component: {
+        tips: {
+            title: "提示",
+            close: "我也是汉字",
+            close1: "我也是汉字22",
+            close2: "我也是汉字33",
+        },
+        v_login: {
+            titleM: "我Mm",
+            titleA: "登陆",
+            status: {
+                en:'英文',
+                zh:'中文'
+            }
+        }
+    },
+    v_cadCheckPendingSum: {
+        list: {
+            amount: '数字',
+            payType: '付款类型',
+            createDate: '创建时间',
+            auditStatus: '审核状态',
+            auditReason: '我这里也有汉字，哈哈。'
+        },
+        commonAccount: '技术技术',
+        passYes: '通过',
+        passNo: '失败',
+        passCheck: '哈哈哈，呵呵。',
+        passCheckA: '哈哈哈，呵呵额呵呵呵',
+        passCheckB: '哈哈哈，呵呵。搞笑了'
+    }
+}
+module.exports = lan;
--- main.js
+++ main.js
@@ -3,14 +3,16 @@ const path = require('path');
 const I18N_PATH = path.join(__dirname, 'i18n');
 const I18N_PATH_EN_US = path.join(I18N_PATH, 'en-US.js');
 const I18N_PATH_ZH_CN = path.join(I18N_PATH, 'zh-CN.js');
-const I18N_PATH_TEST = path.join(I18N_PATH, 'test.js');
+const I18N_PATH_TEST_EN = path.join(I18N_PATH, 'test-en.js');
+const I18N_PATH_TEST_ZH = path.join(I18N_PATH, 'test-zh.js');
 const LAN_CN = 'zh-CN';
 const LAN_TW = 'zh-TW';
 const LAN_EN = 'en-US';
 const REG_EXP_CN = /[\u4E00-\u9FA5]/g;
 let enUsJson = require(I18N_PATH_EN_US);
 let zhCnJson = require(I18N_PATH_ZH_CN);
-let testJson = require(I18N_PATH_TEST);
+let testJsonEn = require(I18N_PATH_TEST_EN);
+let testJsonZh = require(I18N_PATH_TEST_ZH);
 let enUsJsonPure = {};
 
 function _isEmptyObject(obj) {
@@ -46,5 +48,21 @@ function fnDeleteEmptyObj(obj) {
     return obj;
 }
 
-let filterJson = fnFilterZhInEn(testJson);
-let pureTestJson = fnDeleteEmptyObj(filterJson);
+let filterTestJson = fnFilterZhInEn(testJsonEn);
+let pureTestJson = fnDeleteEmptyObj(filterTestJson);
+// console.log(pureTestJson);
+
+function fnMergeObj(baseObj, obj){
+    for ( let k in baseObj ) {
+        if(obj[k] === undefined) {
+            obj[k] = baseObj[k];
+        } else if (typeof obj[k] === 'object') {
+            fnMergeObj(baseObj[k], obj[k]);
+        }
+    }
+    return obj;
+}
+// console.log(testJsonZh);
+let newTestJsonEn = fnMergeObj(testJsonZh, pureTestJson);
+console.log(newTestJsonEn);
+console.log(newTestJsonEn.component.v_login.status);
